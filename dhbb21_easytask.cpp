d66741e726579d2021dd88ea65b598c0414c4992
๖ۣۜŇŇ_MyTツ <myt@nonamestudio.xyz>
2021-09-09 21:16:45 +0700

    updated

---
+++ dhbb21_easytask.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> n;
+    int a[n];
+    if(n>=2) {
+        for (int i=1;i<=n;i++) {
+            cin >> a[i];
+
+        }
+    }
+    else {
+        return 0;
+    }
+    return 0;
+}
